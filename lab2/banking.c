cbcd96afc2ad6693be35420b87bf9e5c04c8c830
TheSilenceOfMind <baladkirill@mail.ru>
2018-05-13 10:35:44 +0300

    fix banking.c

--- lab2/banking.c
+++ lab2/banking.c
@@ -12,7 +12,6 @@ void transfer(void * parent_data, local_id src, local_id dst, balance_t amount)
     if (parent_data == NULL)
         return;
     IO* io = (IO*) parent_data;
-
     if (src <= 0 || dst <= 0 || src > io->proc_number || dst > proc_number)
         return;
     if (src == dst)
@@ -29,31 +28,31 @@ void transfer(void * parent_data, local_id src, local_id dst, balance_t amount)
     Message msg = {{0}};
     msg.s_header = (MessageHeader) {
         .s_magic = MESSAGE_MAGIC,
-        .s_payload_len = 0,  // !!!!!
+        .s_payload_len = sizeof(transfer),
         .s_type = TRANSFER,
         .s_local_time = get_physical_time()
     }
     size_t payload_len = sizeof(TransferOrder);
-    char payload[payload_len];
-    char* ret = memcpy(payload, &transfer, payload_len);
+    char* ret = memcpy(msg.s_payload, &transfer, payload_len);
+#ifdef DEBUG
     if (ret == NULL) {
-        printf("ERROR while memcpy in transfer!\n");
+        fprintf(stderr, "memcpy error in transfer of parent! from %d to %d.\n",
+            src, dst);
     }
+#endif
 
     send((void*)io, src, (const Message *)&msg);
 
     // wait for ACK
     Message ret_msg = {{0}};
-    while(true) {
+    while(1) {
         res = receive((void*)io, dst, &ret_msg);
         if (res != 0)
             continue;
         if (ret_msg->s_header.s_type == ACK) {
-            fprintf(
-                io->pipes_log_stream,
-                "Transfer from %d to %d succeed!\n",
-                src, dst);
-            return;
+            fprintf(stderr, "%d: process 0 received ACK from %d.\n",
+                get_physical_time(), dst);
+            break;
         } else {
             // do whatever is needed if received message isn't ACK
         }
